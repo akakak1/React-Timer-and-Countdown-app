b1ead79fad4e803651cb6984c93585ca8fc39ae6
Amit <Amit@example.com>
2018-01-13 16:32:44 +0530

    Timer and Countdown added

--- public/bundle.js
+++ public/bundle.js
@@ -105,17 +105,24 @@
 
 
 	var Main = __webpack_require__(223);
+	var Timer = __webpack_require__(232);
+	var Countdown = __webpack_require__(225);
 
 	//Load foundation
-	__webpack_require__(225);
+	__webpack_require__(226);
 	$(document).foundation();
 
-	__webpack_require__(229);
+	__webpack_require__(230);
 
 	ReactDOM.render(React.createElement(
 	  Router,
 	  { history: hashHistory },
-	  React.createElement(Route, { path: '/', component: Main })
+	  React.createElement(
+	    Route,
+	    { path: '/', component: Main },
+	    React.createElement(Route, { path: 'countdown', component: Countdown }),
+	    React.createElement(IndexRoute, { component: Timer })
+	  )
 	), document.getElementById('app'));
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(7)))
 
@@ -24983,7 +24990,7 @@
 	                    null,
 	                    React.createElement(
 	                        Link,
-	                        { to: '/', activeClassName: 'active-link' },
+	                        { to: '/countdown', activeClassName: 'active-link' },
 	                        'Countdown'
 	                    )
 	                )
@@ -25016,13 +25023,35 @@
 /* 225 */
 /***/ (function(module, exports, __webpack_require__) {
 
+	'use strict';
+
+	var React = __webpack_require__(8);
+
+	var Countdown = React.createClass({
+	    displayName: 'Countdown',
+
+	    render: function render() {
+	        return React.createElement(
+	            'p',
+	            null,
+	            'Countdown.jsx'
+	        );
+	    }
+	});
+
+	module.exports = Countdown;
+
+/***/ }),
+/* 226 */
+/***/ (function(module, exports, __webpack_require__) {
+
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(226);
+	var content = __webpack_require__(227);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(228)(content, {});
+	var update = __webpack_require__(229)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -25039,10 +25068,10 @@
 	}
 
 /***/ }),
-/* 226 */
+/* 227 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(227)();
+	exports = module.exports = __webpack_require__(228)();
 	// imports
 
 
@@ -25053,7 +25082,7 @@
 
 
 /***/ }),
-/* 227 */
+/* 228 */
 /***/ (function(module, exports) {
 
 	/*
@@ -25109,7 +25138,7 @@
 
 
 /***/ }),
-/* 228 */
+/* 229 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	/*
@@ -25363,16 +25392,16 @@
 
 
 /***/ }),
-/* 229 */
+/* 230 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(230);
+	var content = __webpack_require__(231);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(228)(content, {});
+	var update = __webpack_require__(229)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -25389,18 +25418,40 @@
 	}
 
 /***/ }),
-/* 230 */
+/* 231 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(227)();
+	exports = module.exports = __webpack_require__(228)();
 	// imports
 
 
 	// module
-	exports.push([module.id, ".top-bar, .top-bar ul {\n  background-color: #333333; }\n\n.top-bar .menu-text {\n  color: white; }\n\n.top-bar .menu > .menu-text > a {\n  display: inline;\n  padding: 0px; }\n", ""]);
+	exports.push([module.id, ".top-bar, .top-bar ul {\n  background-color: #333333; }\n\n.top-bar .menu-text {\n  color: white; }\n\n.top-bar .menu > .menu-text > a {\n  display: inline;\n  padding: 0px; }\n\n.top-bar .active-link {\n  font-weight: bold; }\n", ""]);
 
 	// exports
 
+
+/***/ }),
+/* 232 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(8);
+
+	var Timer = React.createClass({
+	    displayName: 'Timer',
+
+	    render: function render() {
+	        return React.createElement(
+	            'p',
+	            null,
+	            'Timer.jsx'
+	        );
+	    }
+	});
+
+	module.exports = Timer;
 
 /***/ })
 /******/ ]);
